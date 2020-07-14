8c0f50ca5b4b5a6145ef0869710d6adcb3e584d9
ZachKellogg <zachskellogg@gmail.com>
2020-07-13 23:07:11 -0600

    forgot to save service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -11,7 +11,6 @@ const FILES_TO_CACHE = [
   const CACHE_NAME = "static-cache-v2";
   const DATA_CACHE_NAME = "data-cache-v1";
   
-  // install
   self.addEventListener("install", function(evt) {
     evt.waitUntil(
       caches.open(CACHE_NAME).then(cache => {
@@ -23,7 +22,6 @@ const FILES_TO_CACHE = [
     self.skipWaiting();
   });
   
-  // activate
   self.addEventListener("activate", function(evt) {
     evt.waitUntil(
       caches.keys().then(keyList => {
@@ -48,7 +46,6 @@ const FILES_TO_CACHE = [
         caches.open(DATA_CACHE_NAME).then(cache => {
           return fetch(evt.request)
             .then(response => {
-              // If the response was good, clone it and store it in the cache.
               if (response.status === 200) {
                 cache.put(evt.request.url, response.clone());
               }
@@ -56,7 +53,6 @@ const FILES_TO_CACHE = [
               return response;
             })
             .catch(err => {
-              // Network request failed, try to get it from the cache.
               return cache.match(evt.request);
             });
         }).catch(err => console.log(err))
